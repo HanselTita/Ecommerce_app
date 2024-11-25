544575bcb5cb99e9d2bf6c575374c42775b9afab
Hansel <hanseltita@gmail.com>
2024-11-25 11:12:12 +0700

    Feat: SafeAreaView in App.js

--- react_native_ecommerce/App.js
+++ react_native_ecommerce/App.js
@@ -1,40 +1,14 @@
-import { Text, View, StyleSheet } from "react-native"
+import {SafeAreaView } from "react-native"
 import WelcomeView from "./src/components/WelcomeView"
 
 export default function App() {
   return (
-    <View style={styles.container}>
+   <SafeAreaView>
       <WelcomeView />
-
-      <View style={styles.box}>
-        <Text style={{ fontSize: 20 }}>Hello</Text>
-        <Text style={{ fontSize: 22 }}>Let's program in</Text>
-        <Text style={{ fontSize: 25 }}>React Native</Text>
-      </View>
-    </View>
+   </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    flex: 1,
-    alignItems: "center",
-
-    backgroundColor: "#bbb",
-  },
-
-  box: {
-    flexDirection: "column",
-
-    backgroundColor: "#ddd",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    marginTop: 10,
-    padding: 40,
-  },
-})
 
 /**Creating a component and running it in the default component
  * The created component must return jxx.
@@ -44,3 +18,6 @@ const styles = StyleSheet.create({
 /** margin give you spacing outside your view while padding gives you spaving within your view */
 
 /**Adding touchableOpacity with replaces onClick of javascript */
+
+/** It is cleaner to use SafeAreaView, which comes with its own components
+ *  and remove the View Items */
