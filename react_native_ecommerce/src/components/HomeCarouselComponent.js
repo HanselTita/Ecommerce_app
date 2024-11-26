7dcf6744c2f1eaaf10aa5a5d1d8c313f6f57c011
Hansel <hanseltita@gmail.com>
2024-11-26 12:11:23 +0700

    Feat: FlatList

--- react_native_ecommerce/src/components/HomeCarouselComponent.js
+++ react_native_ecommerce/src/components/HomeCarouselComponent.js
@@ -1,20 +1,44 @@
-import { Image, View } from "react-native"
+import { FlatList, Image, View } from "react-native"
 import { theme } from "./theme"
+
+const myArray = [
+  {id:"1", title:"item 1"},
+  {id:"2", title:"item 2"},
+  {id:"3", title:"item 3"},
+  {id:"4", title:"item 4"},
+  {id:"5", title:"item 5"},
+]
 
 const HomeCarouselComponent = () => {
 
   return (
-    <View style={{height:200,
+    <View>
+      <View style={{height:200,
      backgroundColor:"#bbb", 
      marginHorizontal:theme.sizes.medium,
      borderRadius:theme.sizes.medium,
+     justifyContent:"center",
      overflow:"hidden"}}>
     <Image style={{resizeMode:"cover", height:"100%", width:"100%"}}source={require("../../assets/img/fn1.jpg")} />
      </View>
+     <View>
+      <FlatList
+      data={myArray}
+      renderItem={()=>{
+  return ( <Text>Hello </Text>
+
+          )
+        }}
+        />
+     </View>
+    </View>
   )
 }
 
 export default HomeCarouselComponent
 
 
-/** overflow is used to cut out those parts of a content that overlap their boundaries. */
+/** overflow is used to cut out those parts of a content that overlap their boundaries. 
+ * Flatlist is like a loop that cycles the according to the number of item given.
+ * In a Flatlist the data is the array, the renderItem returns the component
+*/
