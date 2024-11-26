2ebe59ad89679aef666acc017f241fe5249ff4da
Hansel <hanseltita@gmail.com>
2024-11-26 15:01:20 +0700

    Feat: keyExtrator

--- react_native_ecommerce/src/components/HomeCarouselComponent.js
+++ react_native_ecommerce/src/components/HomeCarouselComponent.js
@@ -24,11 +24,8 @@ const HomeCarouselComponent = () => {
      <View>
       <FlatList
       data={myArray}
-      renderItem={({item})=>{
-  return ( <Text>{item.title} </Text>
-
-          )
-        }}
+      renderItem={({item})=><Text>{item.title} </Text>}
+      keyExtractor={item=>item.id}
         />
      </View>
     </View>
@@ -41,5 +38,6 @@ export default HomeCarouselComponent
 /** overflow is used to cut out those parts of a content that overlap their boundaries. 
  * Flatlist is like a loop that cycles the according to the number of item given.
  * In a Flatlist the data is the array, the renderItem returns a component
- * in the component call and item and tell it what to return, in our case "title"
+ * in the component call and item /prop and tell it what to return, in our case "title"
+ * it is good practice to include keyExtrator, to help in your app caching
 */
