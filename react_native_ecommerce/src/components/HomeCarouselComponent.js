d2c06fa1ce764f5fd9f9141f8cdcf39564c90e0a
Hansel <hanseltita@gmail.com>
2024-11-26 09:08:13 +0700

    HomeCarouselComponent: Put image in View for resizing - resizemode - overflow.

--- react_native_ecommerce/src/components/HomeCarouselComponent.js
+++ react_native_ecommerce/src/components/HomeCarouselComponent.js
@@ -1,6 +1,20 @@
-import { Image } from "react-native"
+import { Image, View } from "react-native"
+import { theme } from "./theme"
+
 const HomeCarouselComponent = () => {
-  return <Image source={require("../../assets/img/fn1.jpg")} />
+
+  return (
+    <View style={{height:200,
+     backgroundColor:"#bbb", 
+     marginHorizontal:theme.sizes.medium,
+     borderRadius:theme.sizes.medium,
+     overflow:"hidden"}}>
+    <Image style={{resizeMode:"cover", height:"100%", width:"100%"}}source={require("../../assets/img/fn1.jpg")} />
+     </View>
+  )
 }
 
 export default HomeCarouselComponent
+
+
+/** overflow is used to cut out those parts of a content that overlap their boundaries. */
