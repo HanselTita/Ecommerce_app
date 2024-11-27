e44d05bbe16589e03565429a471e0ce8cfdbd46c
Hansel <hanseltita@gmail.com>
2024-11-27 11:07:30 +0700

    feat: showHorizontalIndicator-snapToAligment-decelerationRate

--- react_native_ecommerce/src/components/HomeCarouselComponent.js
+++ react_native_ecommerce/src/components/HomeCarouselComponent.js
@@ -10,37 +10,42 @@ const images = [
   
 ]
 
-const myArray = [
-  {id:"1", title:"item 1"},
-  {id:"2", title:"item 2"},
-  {id:"3", title:"item 3"},
-  {id:"4", title:"item 4"},
-  {id:"5", title:"item 5"},
-]
 
 const HomeCarouselComponent = () => {
-
-  return (
-    <View>
-      <View style={{height:200,
-     marginHorizontal:theme.sizes.medium,
-     borderRadius:theme.sizes.medium,
-     justifyContent:"center",
-     overflow:"hidden"}}>
+return (
+  <View>
+    <View
+      style={{
+        height: 200,
+        backgroundColor: theme.colors.offWhite,
+        marginHorizontal: theme.sizes.medium,
+        borderRadius: theme.sizes.medium,
+        overflow: "hidden",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <FlatList
-      data={images}
-      renderItem={({item})=><Image style={{resizeMode:"cover", height:"100%", width:width}}
-      source={images.path} />}
-      keyExtractor={item=>item.id}
-      horizontal
-      pagingEnabled
-        />
-     </View>
+        data={images}
+        renderItem={({ item }) => (
+          <Image
+            style={{ resizeMethod: "cover", width: width, height: "100%" }}
+            source={item.path}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        snapToAlignment="center"
+        decelerationRate="fast"
+      />
     </View>
-  )
-}
+  </View>
+);
+};
 
-export default HomeCarouselComponent
+export default HomeCarouselComponent;
 
 
 /** overflow is used to cut out those parts of a content that overlap their boundaries. 
@@ -48,4 +53,9 @@ export default HomeCarouselComponent
  * In a Flatlist the data is the array, the renderItem returns a component
  * in the component call and item /prop and tell it what to return, in our case "title"
  * it is good practice to include keyExtrator, to help in your app caching
+ * showsHorizontalScrollIndicator is to decide whether you want a bar to show when you
+ * scroll through the images.
+ * snapToAlignment helps the image to be centered when scrolling and not 
+ * showing half of another image
+ * decelerationRate helps to determine the speed of the scrolling
 */
