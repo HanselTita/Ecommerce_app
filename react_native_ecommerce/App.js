9abd7ccb5d2afc330511f007deb02b1625161ebc
Hansel <hanseltita@gmail.com>
2024-11-28 12:34:14 +0700

    safeAreaView-Flatlist - localProductList for ProductListScreen.

--- react_native_ecommerce/App.js
+++ react_native_ecommerce/App.js
@@ -4,8 +4,8 @@ import { NavigationContainer } from "@react-navigation/native"
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import ProductList from "./src/screens/ProductListScreen";
 
-
 const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
  <NavigationContainer>
--- react_native_ecommerce/src/screens/ProductListScreen.js
+++ react_native_ecommerce/src/screens/ProductListScreen.js
@@ -1,14 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 
+const localProductList = [
+  {id:"1A", path: require("../../assets/img/fn5.jpg")},
+  {id:"1B", path: require("../../assets/img/fn3.jpg")},
+  {id:"1C", path: require("../../assets/img/fn2.jpg")},
+  
+]
+
 export default function ProductList() {
+  const renderItem =({item})=>{
+    return (
+      <View style={{width:180, height:180}}>
+      <Image
+      source={item.path}
+    />
+      </View>
+   
+  )}
   return (
-    <View>
-      <Text>ProductList</Text>
-    </View>
+    <SafeAreaView>
+<FlatList
+    data={localProductList}
+    renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+    />
+    </SafeAreaView>
+    
   )
 }
 
 /**Notice we have export default at begining of our function.
  * That means no need to export default at the end of your code
+ * 
  */
